ecadc81672264982b263bd7d672febfad8efffc4
Zsolt Mester <22896196+meister245@users.noreply.github.com>
2020-03-22 04:17:33 +0100

    added sample script

---
+++ scripts/tinder-token.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import sys
+import getpass
+
+import requests
+
+
+def main():
+    requests.urllib3.disable_warnings()
+    auth_type = input(f'choose authentication [facebook/phone]: ')
+
+    if auth_type == 'facebook':
+        from tinder_token.facebook import TinderTokenFacebookV2
+
+        facebook = TinderTokenFacebookV2()
+        fb_email = input('facebook email: ')
+        fb_password = getpass.getpass('facebook password: ')
+
+        token, refresh_token = facebook.get_tinder_token(fb_email=fb_email, fb_password=fb_password)
+
+    elif auth_type == 'phone':
+        from tinder_token.phone import TinderTokenPhoneV2
+
+        phone = TinderTokenPhoneV2()
+        phone_number = input('enter phone number [country_code + phone_number]: ')
+
+        phone.send_otp_code(phone_number)
+        otp_code = input('enter received code: ')
+
+        refresh_token = phone.get_refresh_token(otp_code, phone_number)
+        token = phone.get_tinder_token(refresh_token)
+
+    else:
+        exit(f'invalid value "{auth_type}"')
+
+    print(f'your access token - {token}')
+
+
+if __name__ == "__main__":
+    main()
