70d0a03162b81fe4473daf81d368ddd6541d0547
Zsolt Mester <22896196+meister245@users.noreply.github.com>
2020-03-22 16:19:43 +0100

    updated setup

--- setup.py
+++ setup.py
@@ -4,8 +4,11 @@ import re
 from setuptools import setup
 
 
-with open('./tinder_token/__init__.py') as f:
+with open('./tinder_token/__init__.py', 'r') as f:
     version = re.search(r'(?<=__version__ = .)([\d\.]*)', f.read()).group(1)
+
+with open('./README.md', 'r') as f:
+    readme = f.read()
 
 
 if __name__ == '__main__':
@@ -14,7 +17,14 @@ if __name__ == '__main__':
         version=version,
         author='Zsolt Mester',
         author_email='',
+        description='Python library for generating access tokens for using Tinder API, using Facebook or Phone authentication',
+        long_description=readme,
+        license='MIT',
         url='https://github.com/meister245/tinder-token',
+        project_urls={
+            "Code": "https://github.com/meister245/tinder-token",
+            "Issue tracker": "https://github.com/meister245/tinder-token/issues",
+        },
         packages=[
             'tinder_token'
         ],
@@ -32,6 +42,5 @@ if __name__ == '__main__':
         scripts=[
             'scripts/tinder-token',
             'scripts/tinder-token.py'
-        ],
-        license='MIT'
+        ]
     )
